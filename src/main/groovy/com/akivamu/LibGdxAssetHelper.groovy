726be3ec856f1e3b9d5a3d8e6842610aad76cce1
Akivamu <akivamu@gmail.com>
2017-08-23 12:51:54 +0700

    ini

---
+++ src/main/groovy/com/akivamu/LibGdxAssetHelper.groovy
@@ -0,0 +1,13 @@
+package com.akivamu
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LibGdxAssetHelper implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.task("packTexture") {
+            println("HELLO")
+        }
+    }
+}
